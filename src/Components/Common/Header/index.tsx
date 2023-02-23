903e32f1c78b72ae3213b591422af78a8a8cd7f3
Oksana1892 <Sidorkurs@yandex.ru>
2023-02-23 17:27:52 +0300

    create_shop_2023

---
+++ src/Components/Common/Header/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import style from './header.module.scss';
+
+export const Header = () => {
+  return <header className={style.header}>Header</header>;
+};
---
+++ src/Containers/Main/index.tsx
@@ -0,0 +1,11 @@
+import { Footer } from "Components/Common/Footer";
+import { Header } from "Components/Common/Header";
+import React from "react";
+
+export const Main = () => {
+    return <div>
+        <Header/>
+        <Footer/>
+    </div>;
+  };
+  
